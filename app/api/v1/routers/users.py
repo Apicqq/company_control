e87ea78c52b65f0d2b3fe0648044632aadd92413
Nikita Smykov <132088678+Apicqq@users.noreply.github.com>
2024-12-11 10:13:16 +0300

    refactor: implement "/me/" endpoint

--- app/api/v1/routers/users.py
+++ app/api/v1/routers/users.py
@@ -1,11 +1,28 @@
-from fastapi import APIRouter
+from http import HTTPStatus
 
+from fastapi import APIRouter, Depends, HTTPException
+from fastapi.security import OAuth2PasswordBearer
+from jwt.exceptions import InvalidTokenError
+
+from app.schemas.user import UserIn, UserOut
+from app.api.v1.routers.auth import get_current_auth_user
+from app.services.user import UserService
+from app.utils.auth import decode_jwt
 
 router = APIRouter(
     prefix="/users",
 )
 
 
+
+
 @router.get("/me")
-async def read_users_me():
-    return {"user_id": "the current user"}
+async def check_self_info(
+        current_user: UserIn = Depends(get_current_auth_user),
+) -> UserOut:
+    return UserOut.model_validate(current_user)
+
+
+@router.post("/change-password")
+async def change_password():
+    pass
