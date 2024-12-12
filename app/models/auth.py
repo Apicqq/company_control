54464727db08456f28339e581c338b892bcd78ae
Nikita Smykov <132088678+Apicqq@users.noreply.github.com>
2024-12-12 07:25:00 +0300

    feat: add unique constraint on invitechallange model

--- app/models/auth.py
+++ app/models/auth.py
@@ -1,11 +1,16 @@
 from app.models.base import Base
 
-from sqlalchemy import String
+from sqlalchemy import String, UniqueConstraint
 from sqlalchemy.orm import Mapped, mapped_column
 
 
 class InviteChallenge(Base):
     """Database model for invite challenge."""
+
+    __table_args__ = (
+        UniqueConstraint(
+            "account", "invite_token", name="invite_token_unique"),
+    )
 
     account: Mapped[str] = mapped_column(String)
     invite_token: Mapped[str] = mapped_column(String)
